abb7d08744daa9592d0a56bf5659c4787c323bce
Arnesh Pal <arneshpal000@gmail.com>
2025-02-20 15:32:41 +0530

    Delete EasyER-main/er-diagram-generator directory

--- EasyER-main/er-diagram-generator/prisma/migrations/20250220063322_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Diagram" (
-    "id" TEXT NOT NULL,
-    "svg" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Diagram_pkey" PRIMARY KEY ("id")
-);
